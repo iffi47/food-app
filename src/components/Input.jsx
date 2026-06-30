9b9158b3a17d3521a5160260aebf47916f783033
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-06-30 19:14:39 +0500

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+export default function Input({isTextarea, type, label, errorMessage, id, error, ...props }){
+  return(
+    <>
+    <div className="control no-margin">
+      <label htmlFor={id}>{label}</label>
+      {!isTextarea && <input
+       id={id}
+       type={type}
+       {...props}
+       />}
+       {isTextarea && <textarea
+        id={id}
+        {...props}
+       />}
+      <div className="control-error">{error && <p>{errorMessage}</p>}</div>
+     </div>
+    </>
+  )
+}
