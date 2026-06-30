3dd51ae40e636fc8c4eab0b194f2753a571013de
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-06-30 19:19:09 +0500

    Refactor Input component structure and add Modal component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,23 @@
 export default function Input({isTextarea, type, label, errorMessage, id, error, ...props }){
-  return(
-    <>
+  return (
+   <>
     <div className="control no-margin">
-      <label htmlFor={id}>{label}</label>
-      {!isTextarea && <input
+     <label htmlFor={id}>{label}</label>
+     {!isTextarea && (
+      <input
        id={id}
        type={type}
        {...props}
-       />}
-       {isTextarea && <textarea
-        id={id}
-        {...props}
-       />}
-      <div className="control-error">{error && <p>{errorMessage}</p>}</div>
-     </div>
-    </>
-  )
+      />
+     )}
+     {isTextarea && (
+      <textarea
+       id={id}
+       {...props}
+      />
+     )}
+     <div className="error">{error && <p>{errorMessage}</p>}</div>
+    </div>
+   </>
+  );
 }
