a837913d0df0e4df5a91ad6ff16c020e6856308a
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-07-02 13:20:40 +0500

    Enhance Modal component to accept custom className for styling

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,23 +1,26 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ open, children, onClose }) {
-  const dialog = useRef();
+function Modal({ open, children, onClose, className = "" }) {
+ const dialog = useRef();
 
-  useEffect(() => {
-    if (open) {
-      dialog.current.showModal();
-    } else {
-      dialog.current.close();
-    }
-  }, [open]);
+ useEffect(() => {
+  if (open) {
+   dialog.current.showModal();
+  } else {
+   dialog.current.close();
+  }
+ }, [open]);
 
-  return createPortal(
-    <dialog className="modal" ref={dialog} onClose={onClose}>
-      {open ? children : null}
-    </dialog>,
-    document.getElementById('modal')
-  );
+ return createPortal(
+  <dialog
+   className={`modal ${className}`}
+   ref={dialog}
+   onClose={onClose}>
+   {open ? children : null}
+  </dialog>,
+  document.getElementById("modal"),
+ );
 }
 
 export default Modal;
